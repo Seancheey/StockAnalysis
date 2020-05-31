aff9bfb5873465ccd335529d0f353b0971e07db0
Qiyi Shan <adls371@outlook.com>
2020-05-30 18:13:45 -0700

    handle situation when pipeline has 0 from_series

--- data_collection/pipeline.py
+++ data_collection/pipeline.py
@@ -2,23 +2,22 @@ from typing import List, Union, Callable
 from pandas import DataFrame, Series
 
 
-# Pipeline helper that maps one or two series to a new series
+# Pipeline helper that maps 0-2 series to a new series
 class Pipeline:
     __slots__ = ("from_series", "to_series", "map_func")
 
     def __init__(self, from_series: Union[str, List[str]], to_series: str, map_func: Callable = None):
-        self.from_series = from_series
-        if type(self.from_series) == list:
-            if len(self.from_series) == 1:
-                self.from_series = self.from_series[0]
-            elif len(self.from_series) > 2:
-                raise ValueError("from series allows only up to two elements.")
+        self.from_series = from_series if type(from_series) == list else [from_series]
+        if len(self.from_series) > 2:
+            raise ValueError("from series allows only up to two elements.")
         self.to_series = to_series
         self.map_func = map_func if map_func else lambda x: x
 
     def execute(self, from_df: DataFrame) -> Series:
-        if type(self.from_series) == str:
-            return from_df[self.from_series].map(self.map_func)
+        if len(self.from_series) == 1:
+            return from_df[self.from_series[0]].map(self.map_func)
+        elif len(self.from_series) == 0:
+            return Series(data=[self.map_func()] * len(from_df), name=self.to_series)
         else:
             return from_df[self.from_series[0]].combine(from_df[self.from_series[1]], self.map_func)
 
