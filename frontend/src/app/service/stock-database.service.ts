9cf0aaa938b85f1d100ace972b2026b7d952f714
Qiyi Shan <adls371@outlook.com>
2020-06-28 17:28:11 -0700

    Use switchMap for service

--- frontend/src/app/service/stock-database.service.ts
+++ frontend/src/app/service/stock-database.service.ts
@@ -2,8 +2,8 @@ import {Injectable} from '@angular/core';
 import "protobufjs";
 import {HttpClient} from "@angular/common/http";
 import {Stock} from "./database-entity/Stock";
-import {Observable} from "rxjs";
-import {map} from "rxjs/operators";
+import {Observable, of} from "rxjs";
+import {switchMap} from "rxjs/operators";
 import {StockDailySummary} from "./database-entity/StockDailySummary";
 import {environment} from "../../environments/environment";
 
@@ -17,20 +17,19 @@ export class StockDatabaseService {
 
   getStocks(): Observable<Stock[]> {
     return this.httpClient.jsonp(`${environment.server_url}/get_stocks`, "callback").pipe(
-      map(obj => {
-        console.log("getStock" + obj)
+      switchMap(obj => {
         const stocks: Stock[] = []
         for (let stock of obj["stocks"]) {
           stocks.push(new Stock(stock.code, stock.exchange, stock.fullName))
         }
-        return stocks
+        return of(stocks)
       })
     )
   }
 
   getStockDailyHistory(stock: Stock): Observable<StockDailySummary[]> {
     return this.httpClient.jsonp(`${environment.server_url}/get_stock_hist?exchange=${stock.exchange}&code=${stock.code}`, "callback").pipe(
-      map(obj => {
+      switchMap(obj => {
         const summaries: StockDailySummary[] = []
         for (let summary of obj["dailySummary"]) {
           summaries.push(new StockDailySummary(
@@ -43,7 +42,7 @@ export class StockDatabaseService {
             summary["turnover"],
           ))
         }
-        return summaries
+        return of(summaries)
       })
     )
   }
