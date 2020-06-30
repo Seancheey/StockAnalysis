4116ab5defb2722b9f06e3c0238c69b45b80f9d0
Qiyi Shan <adls371@outlook.com>
2020-06-30 14:27:05 -0700

    Add firebase functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,11 @@
 import * as functions from 'firebase-functions';
+import * as express from "express";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const app = express()
+
+app.get("/get_stocks", (req, res) => {
+    res.redirect("http://qi1.co:5000/get_stocks")
+})
+
+export const stock = functions.https.onRequest(app);
+
